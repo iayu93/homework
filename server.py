255df910619de25752edb621113aa9a01b73e473
yujing <yujing@moyi365.com>
2025-11-13 18:19:13 +0800

    feat:

--- server.py
+++ server.py
@@ -2,47 +2,51 @@ import socket
 import threading
 import operator
 import re
+import queue
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 8080))
 server.listen(5)
 
-ops = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv
-}
+'''
+线程池
+固定线程数量
+闲线程列表
+忙线程列表
 
-def calulator(num1, num2, operator:str, ret):
-    ret['value'] = ops[operator](num1, num2)
+如果有任务，则去闲线程找，没有则阻塞
+忙线程结束后归入闲线程
 
-try:
+复用时线程内容如何动态获取任务内容
+共享空间或者消息队列
+
+长期没有任务时如何处理线程，避免长时占用cpu
+挂起/停掉，来任务后再创建
+'''
+
+tasks = queue.Queue()
+
+def worker():
     while True:
-        print("Waiting for client's connect...")
-        conn, addr = server.accept()
-        print("connected", addr)
-        exps = []
         try:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                data = data.decode()
-                print("client send:", data)
-                m = re.fullmatch(r"\s*(\d+)\s*([+\-*/])\s*(\d+)\s*", data)
-                if not m:
-                    conn.sendall(b"invalid expression\n")
-                    break
-                num1, op, num2 = m.groups()
-                # conn.send(b"I received the data")
-            ret = {}
-            thread = threading.Thread(target=calulator, args=(int(num1), int(num2), op, ret))
-            thread.start();thread.join()
-            conn.sendall(str(ret['value']).encode())
-        finally:
+            conn = tasks.get()
+            data = (conn.recv(1024)).decode()
+            conn.sendall(str(eval(data)).encode())
             conn.close()
-except KeyboardInterrupt:
-    print("shutting down")
-finally:
-    server.close()
+        finally:
+            tasks.task_done()
+    
+def new_threads(num: int = 5):
+    for _ in range(num):
+        threading.Thread(target=worker, daemon=True).start()
+
+
+new_threads()
+while True:
+    try:
+        conn = server.accept()
+        print('server received data')
+        tasks.put(conn)
+    except KeyboardInterrupt:
+        break
+server.close()
