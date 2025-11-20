5fe65361c9c37a5ced3624b09631b969307c3b78
yujing <yujing@moyi365.com>
2025-11-20 18:16:19 +0800

    feat: 支持多个任务处理逻辑

--- client.py
+++ client.py
@@ -1,10 +1,22 @@
 import socket
+import json
 
-for i in range(2):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(5)
-    client.connect(('127.0.0.1', 8080))
-    client.send((f"1+1").encode())
-    data = client.recv(1024)
-    print("server replied: ", data.decode())
-    client.close()
+for _ in range(3):
+    for i in range(2):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(5)
+        client.connect(('127.0.0.1', 8080))
+        row_data = {'handler': 'calculator', 'data': '1+1'}
+        client.send(json.dumps(row_data).encode())
+        data = client.recv(1024)
+        print("server replied: ", data.decode())
+        client.close()
+    for i in range(1):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(5)
+        client.connect(('127.0.0.1', 8080))
+        row_data = {'handler': 'to_lower', 'data': 'YJ'}
+        client.send(json.dumps(row_data).encode())
+        data = client.recv(1024)
+        print("server replied: ", data.decode())
+        client.close()
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import operator
 import re
 import queue
 import time
+import json
 
 '''
 线程池
@@ -20,11 +21,24 @@ import time
 长期没有任务时如何处理线程，避免长时占用cpu
 挂起/停掉，来任务后再创建
 '''
+
+def calculator(conn: socket.socket, expr: str):
+    print(f"calculating {expr}")
+    ret = eval(expr)
+    conn.sendall(str(ret).encode())
+
+def to_lower(conn: socket.socket, data: str):
+    conn.sendall(data.lower().encode())
+
+HANDLERS = {
+    'calculator': calculator,
+    'to_lower': to_lower
+}
+
 class Thread:
-    def __init__(self, handler, task_queue):
+    def __init__(self, task_queue):
         self.stop_event = threading.Event()
         self.free_event = threading.Event()
-        self.handler = handler
         self.tasks = task_queue
         self.thread = threading.Thread(target=self._worker, daemon=True)
 
@@ -38,23 +52,29 @@ class Thread:
             self.thread.join()
 
     def _worker(self):
+        print(f"worker is running, {threading.current_thread().name}")
         while not self.stop_event.is_set():
             try:
                 self.free_event.set()
                 task = self.tasks.get(True, timeout=0.5)
+                print(f"task: {task}")
             except queue.Empty:
                 continue
 
             self.free_event.clear()
 
+            conn, to_handle = task
+            print(f"to handle: {to_handle}")
+            to_handle = json.loads(to_handle) 
+            handler = to_handle['handler']
+            data = to_handle['data'] 
             try:
-                self.handler(*task)
+                HANDLERS[handler](conn, data)
             finally:
                 self.tasks.task_done()
 
         print(f"worker: {threading.current_thread().name} exit")
     
-
 class ThreadPool:
     def __enter__(self):
         return self
@@ -64,9 +84,8 @@ class ThreadPool:
         self.shutdown(wait=True)
         print(f"[Thread pool] __exit__ done")
 
-    def __init__(self, handler, num: int = 5):
+    def __init__(self, num: int = 5):
         self.threads = []
-        self.handler = handler
         self.tasks = queue.Queue()
         self.accepting = True
         self.add_threads(num)
@@ -74,7 +93,7 @@ class ThreadPool:
 
     def add_threads(self, num: int = 1):
         for _ in range(num):
-            thread = Thread(self.handler, self.tasks)
+            thread = Thread(self.tasks)
             self.threads.append(thread)
             thread.start()
 
@@ -104,19 +123,17 @@ class ThreadPool:
         self.tasks.put(args)
 
 def handle_connection(conn: socket.socket, pool: ThreadPool):
-    while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        tid = threading.current_thread().name
-        pool.submit(conn, data)
-        print(f'tid={tid} data={data}')
-        # time.sleep(3)
-    conn.close()
-
-def calculator(conn: socket.socket, expr: str):
-    ret = eval(expr)
-    conn.sendall(str(ret).encode())
+    try:
+        while True:
+            data = conn.recv(2048).decode()
+            if not data:
+                break
+            tid = threading.current_thread().name
+            pool.submit(conn, data)
+            print(f'tid={tid} data={data}')
+            # time.sleep(3)
+    finally:
+        conn.close()
 
 
 def accept_loop(server: socket.socket, pool: ThreadPool):
@@ -124,18 +141,19 @@ def accept_loop(server: socket.socket, pool: ThreadPool):
         try:
             conn, _ = server.accept()
             print('server received data')
-            threading.Thread(target=handle_connection, args=(conn, pool)).start()
+            threading.Thread(target=handle_connection, args=(conn, pool), daemon=True).start()
             # pool2.submit(conn)
             # thread_pool.add_threads(1)
         except KeyboardInterrupt:
             break
 
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 8080))
 server.listen(5)
 
-pool1 = ThreadPool(calculator, 5)
-# pool2 = ThreadPool(socket_calculator, 3)
+pool1 = ThreadPool(3)
 accept_loop(server, pool1)
 pool1.remove_threads(3)
 server.close()
